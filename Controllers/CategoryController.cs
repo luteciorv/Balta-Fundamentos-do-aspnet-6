d2d661470c4c0ccf26f0449a64fd2bdaeaf7226f
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-01 12:00:42 -0300

    Criação do CategoryController e nomeação de um endpoint

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromServices] BlogDataContext context)
+            => Ok(context.Categories.AsNoTracking().ToList());
+    }
+}
