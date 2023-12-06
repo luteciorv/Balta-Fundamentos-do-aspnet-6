85f97830670a3d6129213c2b59e3fe302b9ca0de
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-06 13:45:56 -0300

    Criação do serviço de envio de e-mail

--- Program.cs
+++ Program.cs
@@ -57,6 +57,7 @@ void ConfigureServices(WebApplicationBuilder builder)
 {
     builder.Services.AddDbContext<BlogDataContext>();
     builder.Services.AddTransient<TokenService>();
+    builder.Services.AddTransient<EmailService>();
 }
 
 void LoadConfiguration(WebApplication app)
