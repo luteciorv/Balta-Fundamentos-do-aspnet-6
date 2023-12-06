3ad38bdf8f9a29a7172c5fb4162aeb35aeaa4d24
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-06 11:09:26 -0300

    Implementação de autenticação por ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,7 @@
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "D5D06841C6666AB42ADB57BADFAF3AA4A19E8D40B478C945FFD8C65DC0A31E80";
+    public static string JwtKey = "D5D06841C6666AB42ADB57BADFAF3AA4A19E8D40B478C945FFD8C65DC0A31E80";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_balta_chave_da_api_super_secreta";
 }
