a7883b268087cc1acdf392ebcde43031a6384208
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-06 09:51:33 -0300

    Criação do Token

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+[Route("v1/accounts")]
+public class AccountController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Login([FromServices] TokenService tokenService)
+    {
+        var token = tokenService.GenerateToken(null);
+        return Ok(token);
+    }
+}
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Blog.Services;
@@ -13,6 +14,11 @@ public class TokenService
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new(ClaimTypes.Name, "luis"),
+                new(ClaimTypes.Role, "admin")
+            }),
             Expires = DateTime.UtcNow.AddHours(1),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
         };
