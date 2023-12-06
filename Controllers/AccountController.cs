ff4c84102c705e65ee8a529306d22d836b168278
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-06 10:11:13 -0300

    Testando a autenticação por Roles

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers;
@@ -13,4 +14,19 @@ public class AccountController : ControllerBase
         var token = tokenService.GenerateToken(null);
         return Ok(token);
     }
+
+    [Authorize(Roles = "user")]
+    [HttpGet("user")]
+    public IActionResult GetUser()
+        => Ok(User.Identity?.Name);
+
+    [Authorize(Roles = "author")]
+    [HttpGet("author")]
+    public IActionResult GetAuthor()
+        => Ok(User.Identity?.Name);
+
+    [Authorize(Roles = "admin")]
+    [HttpGet("admin")]
+    public IActionResult GetAdmin()
+        => Ok(User.Identity?.Name);
 }
